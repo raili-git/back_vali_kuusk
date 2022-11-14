9061a2bdb19da45bdcdd29da6422580202f95b67
raili-git <raili.moim@gmail.com>
2022-11-14 14:18:42 +0200

    develop

--- src/main/java/ee/valiit/back_vali_kuusk/HelloWorldController.java
+++ src/main/java/ee/valiit/back_vali_kuusk/HelloWorldController.java
@@ -1,2 +1,13 @@
-package ee.valiit.back_vali_kuusk;public class HElloWorldController {
+package ee.valiit.back_vali_kuusk;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello/world")
+    public String helloWorld() {
+        return "HELLOOOOO WOOORLD!!!!!";
+    }
 }
