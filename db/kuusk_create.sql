9334a6d4bd62a305a008c86fecce88e0c379226f
temp <astrid.oras@gmail.com>
2022-11-23 10:46:27 +0200

    Database updated

--- db/kuusk_create.sql
+++ db/kuusk_create.sql
@@ -1,33 +1,22 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2022-11-22 12:16:21.986
+-- Last modification date: 2022-11-23 07:56:26.026
 
 -- tables
 -- Table: address
 CREATE TABLE address (
                          id serial  NOT NULL,
-                         county varchar(255)  NOT NULL,
-                         x int  NOT NULL,
-                         y int  NOT NULL,
                          street varchar(255)  NOT NULL,
-                         postalcode varchar(255)  NOT NULL,
-                         seller_id int  NOT NULL,
-                         customer_id int  NOT NULL,
-                         county_id int  NOT NULL,
                          CONSTRAINT address_pk PRIMARY KEY (id)
-);
-
--- Table: cart
-CREATE TABLE cart (
-                      id serial  NOT NULL,
-                      customer_id int  NOT NULL,
-                      CONSTRAINT cart_pk PRIMARY KEY (id)
 );
 
 -- Table: contact
 CREATE TABLE contact (
                          id serial  NOT NULL,
+                         address_id int  NOT NULL,
                          phone_number varchar(255)  NOT NULL,
                          email varchar(255)  NOT NULL,
+                         first_name varchar(255)  NOT NULL,
+                         last_name varchar(255)  NOT NULL,
                          CONSTRAINT contact_pk PRIMARY KEY (id)
 );
 
@@ -35,25 +24,14 @@ CREATE TABLE contact (
 CREATE TABLE county (
                         id serial  NOT NULL,
                         name varchar(255)  NOT NULL,
-                        product_id int  NOT NULL,
                         CONSTRAINT county_pk PRIMARY KEY (id)
-);
-
--- Table: customer
-CREATE TABLE customer (
-                          id serial  NOT NULL,
-                          first_name varchar(255)  NOT NULL,
-                          last_name varchar(255)  NOT NULL,
-                          role_id int  NOT NULL,
-                          contact_id int  NOT NULL,
-                          CONSTRAINT customer_pk PRIMARY KEY (id)
 );
 
 -- Table: delivery
 CREATE TABLE delivery (
                           id serial  NOT NULL,
                           type varchar(255)  NOT NULL,
-                          cart_id int  NOT NULL,
+                          price int  NOT NULL,
                           CONSTRAINT delivery_pk PRIMARY KEY (id)
 );
 
@@ -61,18 +39,40 @@ CREATE TABLE delivery (
 CREATE TABLE height (
                         id serial  NOT NULL,
                         gap varchar(255)  NOT NULL,
-                        product_id int  NOT NULL,
                         CONSTRAINT height_pk PRIMARY KEY (id)
+);
+
+-- Table: order
+CREATE TABLE "order" (
+                         id serial  NOT NULL,
+                         delivery_id int  NOT NULL,
+                         status char(1)  NOT NULL,
+                         first_name varchar(255)  NOT NULL,
+                         last_name varchar(255)  NOT NULL,
+                         phone_number int  NOT NULL,
+                         email varchar(255)  NOT NULL,
+                         CONSTRAINT order_pk PRIMARY KEY (id)
+);
+
+-- Table: order_product
+CREATE TABLE order_product (
+                               id serial  NOT NULL,
+                               order_id int  NOT NULL,
+                               product_id int  NOT NULL,
+                               CONSTRAINT order_product_pk PRIMARY KEY (id)
 );
 
 -- Table: product
 CREATE TABLE product (
                          id serial  NOT NULL,
+                         user_id int  NOT NULL,
+                         type_id int  NOT NULL,
+                         address_id int  NOT NULL,
+                         county_id int  NOT NULL,
+                         height_id int  NOT NULL,
                          status char(1)  NOT NULL,
-                         image int  NOT NULL,
+                         image_data bytea  NOT NULL,
                          price int  NOT NULL,
-                         seller_id int  NOT NULL,
-                         cart_id int  NOT NULL,
                          CONSTRAINT product_pk PRIMARY KEY (id)
 );
 
@@ -83,153 +83,119 @@ CREATE TABLE role (
                       CONSTRAINT role_pk PRIMARY KEY (id)
 );
 
--- Table: seller
-CREATE TABLE seller (
-                        id serial  NOT NULL,
-                        username varchar(255)  NOT NULL,
-                        password varchar(255)  NOT NULL,
-                        first_name varchar(255)  NOT NULL,
-                        last_name varchar(255)  NOT NULL,
-                        user_id int  NOT NULL,
-                        role_id int  NOT NULL,
-                        contact_id int  NOT NULL,
-                        CONSTRAINT seller_pk PRIMARY KEY (id)
-);
-
 -- Table: type
 CREATE TABLE type (
                       id serial  NOT NULL,
                       name varchar(255)  NOT NULL,
-                      product_id int  NOT NULL,
                       CONSTRAINT type_pk PRIMARY KEY (id)
 );
 
 -- Table: user
 CREATE TABLE "user" (
                         id serial  NOT NULL,
+                        contact_id int  NULL,
+                        role_id int  NOT NULL,
+                        username varchar(255)  NOT NULL,
+                        password varchar(255)  NOT NULL,
                         CONSTRAINT user_pk PRIMARY KEY (id)
 );
 
 -- foreign keys
--- Reference: address_county (table: address)
-ALTER TABLE address ADD CONSTRAINT address_county
+-- Reference: contact_address (table: contact)
+ALTER TABLE contact ADD CONSTRAINT contact_address
+    FOREIGN KEY (address_id)
+        REFERENCES address (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: order_delivery (table: order)
+ALTER TABLE "order" ADD CONSTRAINT order_delivery
+    FOREIGN KEY (delivery_id)
+        REFERENCES delivery (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: order_product_order (table: order_product)
+ALTER TABLE order_product ADD CONSTRAINT order_product_order
+    FOREIGN KEY (order_id)
+        REFERENCES "order" (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: order_product_product (table: order_product)
+ALTER TABLE order_product ADD CONSTRAINT order_product_product
+    FOREIGN KEY (product_id)
+        REFERENCES product (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: product_address (table: product)
+ALTER TABLE product ADD CONSTRAINT product_address
+    FOREIGN KEY (address_id)
+        REFERENCES address (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: product_county (table: product)
+ALTER TABLE product ADD CONSTRAINT product_county
     FOREIGN KEY (county_id)
         REFERENCES county (id)
         NOT DEFERRABLE
             INITIALLY IMMEDIATE
 ;
 
--- Reference: address_customer (table: address)
-ALTER TABLE address ADD CONSTRAINT address_customer
-    FOREIGN KEY (customer_id)
-        REFERENCES customer (id)
+-- Reference: product_height (table: product)
+ALTER TABLE product ADD CONSTRAINT product_height
+    FOREIGN KEY (height_id)
+        REFERENCES height (id)
         NOT DEFERRABLE
             INITIALLY IMMEDIATE
 ;
 
--- Reference: address_seller (table: address)
-ALTER TABLE address ADD CONSTRAINT address_seller
-    FOREIGN KEY (seller_id)
-        REFERENCES seller (id)
+-- Reference: product_type (table: product)
+ALTER TABLE product ADD CONSTRAINT product_type
+    FOREIGN KEY (type_id)
+        REFERENCES type (id)
         NOT DEFERRABLE
             INITIALLY IMMEDIATE
 ;
 
--- Reference: cart_customer (table: cart)
-ALTER TABLE cart ADD CONSTRAINT cart_customer
-    FOREIGN KEY (customer_id)
-        REFERENCES customer (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
-;
-
--- Reference: county_product (table: county)
-ALTER TABLE county ADD CONSTRAINT county_product
-    FOREIGN KEY (product_id)
-        REFERENCES product (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
-;
-
--- Reference: customer_contact (table: customer)
-ALTER TABLE customer ADD CONSTRAINT customer_contact
-    FOREIGN KEY (contact_id)
-        REFERENCES contact (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
-;
-
--- Reference: customer_role (table: customer)
-ALTER TABLE customer ADD CONSTRAINT customer_role
-    FOREIGN KEY (role_id)
-        REFERENCES role (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
-;
-
--- Reference: delivery_cart (table: delivery)
-ALTER TABLE delivery ADD CONSTRAINT delivery_cart
-    FOREIGN KEY (cart_id)
-        REFERENCES cart (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
-;
-
--- Reference: height_product (table: height)
-ALTER TABLE height ADD CONSTRAINT height_product
-    FOREIGN KEY (product_id)
-        REFERENCES product (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
-;
-
--- Reference: product_cart (table: product)
-ALTER TABLE product ADD CONSTRAINT product_cart
-    FOREIGN KEY (cart_id)
-        REFERENCES cart (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
-;
-
--- Reference: product_seller (table: product)
-ALTER TABLE product ADD CONSTRAINT product_seller
-    FOREIGN KEY (seller_id)
-        REFERENCES seller (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
-;
-
--- Reference: seller_contact (table: seller)
-ALTER TABLE seller ADD CONSTRAINT seller_contact
-    FOREIGN KEY (contact_id)
-        REFERENCES contact (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
-;
-
--- Reference: seller_role (table: seller)
-ALTER TABLE seller ADD CONSTRAINT seller_role
-    FOREIGN KEY (role_id)
-        REFERENCES role (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
-;
-
--- Reference: seller_user (table: seller)
-ALTER TABLE seller ADD CONSTRAINT seller_user
+-- Reference: product_user (table: product)
+ALTER TABLE product ADD CONSTRAINT product_user
     FOREIGN KEY (user_id)
         REFERENCES "user" (id)
         NOT DEFERRABLE
             INITIALLY IMMEDIATE
 ;
 
--- Reference: type_product (table: type)
-ALTER TABLE type ADD CONSTRAINT type_product
-    FOREIGN KEY (product_id)
-        REFERENCES product (id)
+-- Reference: user_contact (table: user)
+ALTER TABLE "user" ADD CONSTRAINT user_contact
+    FOREIGN KEY (contact_id)
+        REFERENCES contact (id)
+        NOT DEFERRABLE
+            INITIALLY IMMEDIATE
+;
+
+-- Reference: user_role (table: user)
+ALTER TABLE "user" ADD CONSTRAINT user_role
+    FOREIGN KEY (role_id)
+        REFERENCES role (id)
         NOT DEFERRABLE
             INITIALLY IMMEDIATE
 ;
 
 -- End of file.
+
+
+
+
+
+
+
+
 
